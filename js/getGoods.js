8851f5565b5b9cea96f3a0578c8c8220673b31ab
i-webtest <stasbass80@gmail.com>
2023-11-30 11:40:40 +0300

    day3

--- js/getGoods.js
+++ js/getGoods.js
@@ -2,12 +2,45 @@ const getGoods = () => {
   // const GOODS_DATA = 'https://wildberries-63d51-default-rtdb.firebaseio.com/db.json';
 
   const links = document.querySelectorAll('.navigation-link');
+  const viewAll = document.querySelector('.more');
 
-  const getData = () => {
+  const renderGoods = (goods) => {
+    const goodsContainer = document.querySelector('.long-goods-list');
+
+    goodsContainer.innerHTML = '';
+
+    goods.forEach((good) => {
+      const goodBlock = document.createElement('div');
+      goodBlock.classList.add('col-lg-3', 'col-sm-6');
+      goodBlock.innerHTML = `
+        <div class="goods-card">
+          <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+          <img src="db/${good.img}" alt="${good.name}" class="goods-image" />
+          <h3 class="goods-title">${good.name}</h3>
+          <p class="goods-description">${good.description}</p>
+          <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+            <span class="button-price">$${good.price}</span>
+          </button>
+        </div>
+      `;
+
+      goodsContainer.append(goodBlock);
+    });
+  };
+
+  const getData = (value, category) => {
     fetch('/db/db.json')
       .then((res) => res.json())
       .then((data) => {
-        localStorage.setItem('goods', JSON.stringify(data));
+        const array = category ? data.filter((item) => item[category] === value) : data;
+
+        localStorage.setItem('goods', JSON.stringify(array));
+
+        if (window.location.pathname !== '/goods.html') {
+          window.location.href = '/goods.html';
+        } else {
+          renderGoods(array);
+        }
       });
   };
 
@@ -15,9 +48,23 @@ const getGoods = () => {
     link.addEventListener('click', (e) => {
       e.preventDefault();
 
-      getData();
+      const linkValue = link.textContent;
+      const category = link.dataset.field;
+
+      getData(linkValue, category);
     });
   });
+
+  if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
+    renderGoods(JSON.parse(localStorage.getItem('goods')));
+  }
+
+  if (viewAll) {
+    viewAll.addEventListener('click', (e) => {
+      e.preventDefault();
+      getData();
+    });
+  }
 };
 
 getGoods();
